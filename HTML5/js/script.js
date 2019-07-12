9bd4279cbc4007c48474bc0aec76ab3e14361f20
abdullaevv <50377227+abdullaevv@users.noreply.github.com>
2019-07-12 19:18:29 +0300

    Add files via upload

---
+++ HTML5/js/script.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+
+	var weather = 'дождливо';
+	var weather = 'пасмwурно';
+
+
+	if (weather == 'дождливо' || weather == 'пасмурно') {
+		alert('Сегодня я лучше посижу дома..');
+	}
+	else{
+		alert('Более менее');
+	}
+
+});
